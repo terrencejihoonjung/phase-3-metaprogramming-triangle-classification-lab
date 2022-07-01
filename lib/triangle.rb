540d2764aa4330e3c25c4fbf76832f1029a5a13c
--replace-all <terrencejung928@gmail.com>
2022-07-01 12:13:44 -0700

    commit tree a98a05361330b5df0c2d0be6299d4bcc96f26876

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+attr_accessor :len1, :len2, :len3
+
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind
+    if self.len1 == 0 || self.len2 == 0 || self.len3 == 0 || self.inequality? == true
+      raise TriangleError
+    elsif self.len1 == self.len2 && self.len2 == self.len3
+      :equilateral
+    elsif self.len1 == self.len2 || self.len1 == self.len3 || self.len2 == self.len3 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
+  private 
+
+  def inequality? 
+    if self.len1 + self.len2 <= self.len3 || self.len2 + self.len3 <= self.len1 || self.len1 + self.len3 <= self.len2
+      true
+    else
+      false
+    end
+  end
+
 end
